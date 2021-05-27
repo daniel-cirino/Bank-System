976c0692c79058b17f83b2e599512fff7ae7e887
Daniel Cirino <dcirinogg@hotmail.com>
2021-05-27 14:09:16 +0100

    Uploading bank system

---
+++ cmake-build-debug/CMakeFiles/Banking_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Banking_System.exe"
+  "Banking_System.exe.manifest"
+  "Banking_System.pdb"
+  "CMakeFiles/Banking_System.dir/main.cpp.obj"
+  "libBanking_System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Banking_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
